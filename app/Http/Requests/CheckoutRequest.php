96c6f91890fe39c6f57ec31763b7544e7b9d7ae5
luisconcha <luisconchacuray@gmail.com>
2016-11-03 20:36:40 -0200

    Criando validação para cupom e items

--- app/Http/Requests/CheckoutRequest.php
+++ app/Http/Requests/CheckoutRequest.php
@@ -11,6 +11,8 @@
  */
 namespace LaccDelivery\Http\Requests;
 
+use Illuminate\Http\Request as HttpRequest;
+
 class CheckoutRequest extends Request
 {
 		/**
@@ -28,16 +30,34 @@ class CheckoutRequest extends Request
 		 *
 		 * @return array
 		 */
-		public function rules()
+		public function rules( HttpRequest $request )
 		{
 				/**
 				 * exists:cupoms,code,used,0
 				 * se_existe:nome_tbl,nome_campo,outro_nome_campo,outro_nome_campo=0
 				 */
-				return [
+				$rules = [
 					'cupom_code' => 'exists:cupoms,code,used,0',
-//					'items.0.product_id' => 'required',
-//					'items.0.qtd'        => 'required',
 				];
+				$this->buildRulesItems( 0, $rules );
+				$items = $request->get( 'items', [] );
+				$items = !is_array( $items ) ? [] : $items;
+				foreach ( $items as $key => $val ):
+						$this->buildRulesItems( $key, $rules );
+				endforeach;
+
+				return $rules;
+		}
+
+		/**
+		 * Metodo responsável por criar as regras para as todas as linha de items que
+		 * forem enviadaspara o servidor podem ser no mínimo 1
+		 * @param       $key
+		 * @param array $rules
+		 */
+		public function buildRulesItems( $key, array &$rules )
+		{
+				$rules[ "items.$key.product_id" ] = 'required';
+				$rules[ "items.$key.qtd" ]        = 'required';
 		}
 }
