e9c5c1c6fa66a8aec446fe5feace90b13c88685b
luisconcha <luisconchacuray@gmail.com>
2016-11-05 20:12:00 -0200

    validações do client

--- app/Http/Requests/AdminClientRequest.php
+++ app/Http/Requests/AdminClientRequest.php
@@ -1,30 +1,30 @@
 <?php
-
 namespace LaccDelivery\Http\Requests;
 
 use LaccDelivery\Http\Requests\Request;
 
 class AdminClientRequest extends Request
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
+		/**
+		 * Determine if the user is authorized to make this request.
+		 *
+		 * @return bool
+		 */
+		public function authorize()
+		{
+				return true;
+		}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+		/**
+		 * Get the validation rules that apply to the request.
+		 *
+		 * @return array
+		 */
+		public function rules()
+		{
+				return [
+					'user.name'  => 'required',
+					'email' => 'required|email|unique:users',
+				];
+		}
 }
